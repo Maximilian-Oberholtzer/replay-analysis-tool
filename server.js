d2fc99d6ae150aee980bb98499669cd68a1e3060
FeedRL <max.oberholtzer@yahoo.com>
2021-01-20 00:40:17 -0500

    changed sever settings

--- server.js
+++ server.js
@@ -6,10 +6,19 @@ const app = express();
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'Frontend/build')));
 
+if(process.env.NODE_ENV === 'production'){
+  //set static folder
+  app.use(express.static('Frontend/build'));
+}
+
 // Put all API endpoints under '/api'
 app.get('/', (req, res) => {
     res.send('Hello World!')
   })
+
+app.get('*', (request, response) => {
+  response.sendFile(path.join(__dirname, 'Frontend/build', 'index.html'));
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port);
